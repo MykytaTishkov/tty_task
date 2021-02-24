30db32845b7ebebb2341ae3907ad6b1be39b3614
MykytaTishkov <mykyta.tishkov@sigma.software>
2021-02-24 08:37:27 +0200

    Adding responses on different ttyX

--- tty_write.c
+++ tty_write.c
@@ -5,17 +5,18 @@
 int main (int argc, char **argv)
 {
     
-    char* tty_addr = calloc(20, sizeof(char));
-    tty_addr = strcat("/dev/", argv[1]);
+    char tty_addr[9];
+    snprintf(tty_addr, 10, "/dev/%s", argv[1]);
+    
     
     FILE* tty = fopen(tty_addr, "w");
     
-    printf("%s\n", tty_addr);
+    //printf("%s\n", tty_addr);
     for (int i = 2; i < argc; i++)
     {
         fprintf(tty, "%s", argv[i]);
     }
-    fprintf(tty, "\n");
-    free(tty_addr);
+    //fprintf(tty, "\n");
+    //free(tty_addr);
     fclose(tty);
 }
